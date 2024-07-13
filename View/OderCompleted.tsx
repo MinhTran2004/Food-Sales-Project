8ab0f34b85e5ac4b5010c956a42eb71aacd693c0
TranMinh2004 <tranminh209204@gmail.com>
2024-07-13 08:50:12 +0700

    Hieu ung load data khi cho du lieu

--- View/OderCompleted.tsx
+++ View/OderCompleted.tsx
@@ -42,7 +42,7 @@ export default function OderCompleted({ navigation }: any) {
     const renderItem = ({ item }: any) => {
         return (
             item.anhsp ? (
-                <View style={{ flexDirection: 'row', flex: 1, padding: 10 }}>
+                <View style={{ flexDirection: 'row', flex: 1, padding: 10, backgroundColor:"#1f222a" }}>
                     <View>
                         <Image source={{ uri: item.anhsp }} style={[style.image_item, { height: 100, width: 100 }]} />
                     </View>
@@ -71,7 +71,7 @@ export default function OderCompleted({ navigation }: any) {
 
     const vertiRender = ({ item }: any) => {
         return (
-            <View style={{ flex: 1, marginBottom: 20 }}>
+            <View style={{ flex: 1, marginBottom: 20, backgroundColor: "#1f222a"}}>
                 <FlatList
                     data={item.cart}
                     renderItem={renderItem} />
